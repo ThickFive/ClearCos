83080addd4749c1d0f3b7acc5d40efda3d48cf8b
ThickFive <thickfive@live.com>
2020-04-05 16:41:20 +0800

    修复 npm run build 打包出现的错误

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
 module.exports = {
+    target: 'node',
     entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        library: 'ClearCos',
+        libraryTarget: 'umd'
+    },
+    node: {
+        __filename: false,
+        __dirname: false,
     }
 }
